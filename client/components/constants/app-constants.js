3742804d1044b0ab55b501ef74439754f1ea973a
Nafeu Nasir <nafeu.nasir@gmail.com>
2018-09-11 01:29:50 -0400

    Updated colors, constants and banner

--- client/components/constants/app-constants.js
+++ client/components/constants/app-constants.js
@@ -2,5 +2,5 @@
 
 app.value('APP_VERSION', '0.1');
 app.value('APP_STORAGE_ID', 'fsproto');
-app.value('APP_NAME', 'Fullstack Prototyper');
-app.value('APP_NAME_LONG', 'Fullstack Prototyper Full Descriptive Title');
+app.value('APP_NAME', 'VMPAC-R');
+app.value('APP_NAME_LONG', 'Verbal Motor Production Assessment for children (revised edition)');
